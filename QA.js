ac4ff2be1cbf51747554f9a5db8bd68c1dccf0f9
soundarrajan <mailforsoundar@gmail.com>
2024-09-20 00:22:07 +0530

    qa added

--- QA.js
+++ QA.js
@@ -220,3 +220,185 @@ console.log([]+[]) // empty string
 console.log(greet()); // undefined js will add ; at the end of return
 
 ////////////////////////
+let randomval = 23;
+if(!typeof randomval === 'string'){
+console.log('not a string');
+}else{
+console.log('its string');
+}
+/*
+typeof randomval  'number'
+!'number' = false;
+false === 'string' // false
+
+Boolean('') // false
+Boolean('number') // true
+*/
+///////////////////////////////
+const userc = {
+    email: 'myemail@gmail.com',
+    updateEmail:(email) =>{
+    this.email = email;
+    }
+    }
+    user.updateEmail('newEmail@gmail.com')
+    console.log(userc.email);
+
+    // arrow function lexical scope is parent here window.
+    // so it add email property in window, not in userc obj
+    ///////////////////////////////////////////
+
+    const animals ={};
+    let dog ={sound:'bark'};
+    let cat ={sound:'meow'};
+    animals[dog]={...dog,name:'dog'};
+    animals[cat]={...cat,name:'cat'};
+    console.log(animals[dog]);
+
+    //{sound: 'meow', name: 'cat'}
+    //console.log(animals);
+    //[object Object]: {sound: 'meow', name: 'cat'}[[Prototype]]: Object
+
+    ///////////////////////////////////////////
+    function sum(a,b){
+        if(b != undefined){
+            return a+b;
+        }else{
+            return function(b){
+                return a+b;
+            }
+        }
+    }
+    
+    console.log(sum(1,2));
+    console.log(sum(1)(2));
+    ///////////////////////////////
+    console.log(1+"2"+"2")
+console.log(1+ +"2"+"2")
+console.log(1+ -"1" + "2")
+console.log(+"1"+"1"+"2")
+console.log("A"-"B"+"2")
+console.log("A"-"B"+2)
+
+// 122
+// 32
+//02
+//112
+//NAN2
+//NAN
+
+///////////////////////////////////////
+
+//TYPE COERSION:
+
+//  + -> convert the other operance to STRING and concateenate
+console.log('5' + 2); // '52' (String concatenation)
+console.log(5 + '2'); // '52' (String concatenation)
+
+// - -> ( -, *, /), JavaScript tries to convert both operands to numbers.
+console.log('5' - 2);  // 3 ('5' is coerced into a number)
+console.log('5' * 2);  // 10 ('5' is coerced into a number)
+console.log('10' / 2); // 5 ('10' is coerced into a number)
+
+
+// Unary Plus (+) - used to convert a value to a number.
+console.log(+ '5'); // 5 (string '5' is coerced to a number)
+console.log(+ true); // 1 (boolean true is coerced to a number)
+console.log(+ false); // 0 (boolean false is coerced to a number)
+
+console.log('hello' - 1);  // NaN ('hello' cannot be coerced into a number)
+console.log(undefined + 2); // NaN (undefined cannot be coerced into a number)
+
+console.log(null + 2); // 2 (null is coerced into 0)
+console.log(undefined + 2); // NaN (undefined cannot be coerced into a number)
+
+console.log(+5);          // 5 (already a number)
+console.log(+'5');        // 5 (string '5' is converted to number)
+console.log(+true);       // 1 (true is coerced to 1)
+console.log(+false);      // 0 (false is coerced to 0)
+console.log(+null);       // 0 (null is coerced to 0)
+console.log(+undefined);  // NaN (undefined cannot be converted to a number)
+
+console.log(-5);          // -5 (negates the number)
+console.log(-'5');        // -5 (string '5' is converted to number, then negated)
+console.log(-true);       // -1 (true is coerced to 1, then negated)
+console.log(-false);      // 0 (false is coerced to 0, no change)
+console.log(-null);       // 0 (null is coerced to 0, no change)
+console.log(-undefined);  // NaN (undefined cannot be converted to a number)
+
+// BOOLEAN
+
+console.log(!true);      // false
+console.log(!false);     // true
+console.log(!0);         // true (0 is falsy)
+console.log(!'');        // true (empty string is falsy)
+console.log(!1);         // false (1 is truthy)
+console.log(!'hello');   // false (non-empty string is truthy)
+
+console.log(!!null); // false
+
+
+console.log(!!null); // false
+console.log(!!''); // flase
+console.log(!!1); //true
+
+console.log(+false); //0
+console.log(!'ddddd'); // false
+////////////////////////////////////////////////////////
+
+let person = {name:'test'};
+let members = [person];
+person = 'ee';
+console.log(members);
+
+// [ { name: 'test' } ]
+//////////////////////////
+
+function getArgs(one, two, three){
+	console.log(one,two,three);
+}
+
+const str1 = "Hello";
+const str2 = "World"
+
+getArgs`${str1} is ${str2}`;
+
+// why the output for this function
+// ["", " is ", ""], "Hello", "World" 
+/////////////////////////////////////////////////////////////////
+
+const a = {};
+const b= {a:1};
+const c = {c:3};
+
+a[b]=123;
+console.log(a); // { '[object Object]': 123 }
+a[c]=456;
+console.log(a); //{ '[object Object]': 456 }
+console.log(a[b]);
+///////////////////////////
+
+const person1 = {
+    name:'ss',
+    hobbies: ['coding']
+    }
+    
+    function addHobby(hobby,hobbies = person1.hobbies){
+    hobbies.push(hobby)
+    return hobbies
+    }
+    
+    addHobby('run',[]);
+    addHobby('dance');
+    addHobby('bake',person1.hobbies);
+    //[ 'coding', 'dance', 'bake' ]
+    //////////////////////////////////////////////////////////////
+
+    const arrayss =[1,2,3,4];
+for(let item in arrayss){
+    console.log(item) // 0 1 2 3
+}
+for(let item of arrayss){
+    console.log(item) //1 2 3 4
+}
+
